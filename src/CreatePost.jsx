e3243b976bb75bf7b31e16d4c7dd95c5e726ed95
pratiksagare <79927151+pratiksagare@users.noreply.github.com>
2024-01-17 12:33:23 +0530

    added create post route

--- src/CreatePost.jsx
+++ src/CreatePost.jsx
@@ -1,6 +1,10 @@
 import { FileInputButton, FileCard } from "@files-ui/react";
+import axios from "axios";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 export const CreatePost = () => {
+  const [postBodyTxt, setPostBodyTxt] = useState('');
+  const uuid = uuidv4();
   const [files, setFiles] = useState([]);
   const updateFiles = (incommingFiles) => {
     //do something with the files
@@ -10,55 +14,69 @@ export const CreatePost = () => {
   const removeFile = (id) => {
     setFiles(files.filter((x) => x.id !== id));
   };
+  const handleCreatePost = async (e) => {
+    e.preventDefault();
+    try {
+      await axios
+        .put(`https://u8jih4ctnh.execute-api.us-east-1.amazonaws.com/createPost`, {
+          postId: uuid,
+          postBody: postBodyTxt,
+          likes: [],
+          comments: [],
+          likeCount: 0,
+          commentCount: 0,
+          postComments: []
+        })
+        .then((response) => {
+          // setTodosList(response.data.Items);
+          console.log(response.data)
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
       <div className="mt-2">
-        <textarea
-          id="message"
-          rows="3"
-          className="outline-none resize-none border-none block p-2.5 w-full text-sm  bg-[#1e1e1e] text-white rounded-lg "
-          placeholder="Write your thoughts here..."
-        ></textarea>
+        <form onSubmit={handleCreatePost}>
+          <textarea
+            id="message"
+            rows="3"
+            // name="postBodyTxt"
+            onChange={e => setPostBodyTxt(e.target.value)}
+            className="outline-none resize-none border-none block p-2.5 w-full text-sm  bg-[#1e1e1e] text-white rounded-lg "
+            placeholder="Write your thoughts here..."
+          ></textarea>
 
-        <div className="flex items-center">
-          <button className="bg-sky-600 text-white px-4 py-1 my-2 hover:cursor-pointer font-semibold">
-            Post
-          </button>
-          {/* <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="30"
-            height="30"
-            fill="currentColor"
-            className="bi bi-camera ml-5  hover:cursor-pointer text-sky-600"
-            viewBox="0 0 16 16"
-          >
-            <path d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4z" />
-            <path d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5m0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7M3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0" />
-          </svg> */}
-          {/* <FileInputButton onChange={updateFiles} value={files} /> */}
-          <FileInputButton onChange={updateFiles} value={files} />
-          {files.length > 0 && (
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                flexWrap: "wrap",
-                gap: "5px",
-                minWidth: "50%",
-              }}
-            >
-              {files.map((file) => (
-                <FileCard
-                  key={file.id}
-                  {...file}
-                  onDelete={removeFile}
-                  info
-                  preview
-                />
-              ))}
-            </div>
-          )}
-        </div>
+          <div className="flex items-center">
+            <button className="bg-sky-600 text-white px-4 py-1 my-2 hover:cursor-pointer font-semibold mr-3" type="submit">
+              Post
+            </button>
+            <FileInputButton onChange={updateFiles} value={files} />
+            {files.length > 0 && (
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "center",
+                  flexWrap: "wrap",
+                  gap: "5px",
+                  minWidth: "50%",
+                }}
+              >
+                {files.map((file) => (
+                  <FileCard
+                    key={file.id}
+                    {...file}
+                    onDelete={removeFile}
+                    info
+                    preview
+                  />
+                ))}
+              </div>
+            )}
+          </div>
+        </form>
       </div>
     </>
   );
